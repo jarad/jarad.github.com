c8ba4959403e58b6b6cbb38cc1679acac07e0281
Jarad Niemi <niemi@iastate.edu>
2013-09-12 14:59:44 -0500

    added posts using knitpages

--- _R/knitpages.R
+++ _R/knitpages.R
@@ -1,24 +1,24 @@
 #!/usr/bin/Rscript --vanilla
 
-# compiles all .Rmd files in _R directory into .md files in Pages directory,
+# compiles all .Rmd files in _R directory into .md files in _posts directory,
 # if the input file is older than the output file.
 
-# run ./knitpages.R to update all knitr files that need to be updated.
+# run ./knitpages.R to update all R markdown files
 
 KnitPost <- function(input, outfile, base.url="/") {
     # this function is a modified version of an example here:
     # http://jfisher-usgs.github.com/r/2012/07/03/knitr-jekyll/
     require(knitr);
     opts_knit$set(base.url = base.url)
-    fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+    fig.path <- paste0("../figs/", sub(".Rmd$", "", basename(input)), "/")
     opts_chunk$set(fig.path = fig.path)
     opts_chunk$set(fig.cap = "center")
     render_jekyll()
     knit(input, outfile, envir = parent.frame())
 }
 
-for (infile in list.files("_R", pattern="*.Rmd", full.names=TRUE)) {
-    outfile = paste0("pages/", sub(".Rmd$", ".md", basename(infile)))
+for (infile in list.files(".", pattern="*.Rmd", full.names=TRUE)) {
+    outfile = paste0("../_posts/", sub(".Rmd$", ".md", basename(infile)))
 
     # knit only if the input file is the last one modified
     if (!file.exists(outfile) |
