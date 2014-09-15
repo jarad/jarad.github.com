f87ac1c662c3cd9397075abe3579d1a6032d0d7f
Jarad Niemi <niemi@iastate.edu>
2014-09-15 13:31:41 -0500

    added histograms and density plots in R code

--- courses/stat401A/lab/ch03.R
+++ courses/stat401A/lab/ch03.R
@@ -1,4 +1,4 @@
-setwd("U:\\401A\\sleuth3csv") # or whever your data are
+setwd("U:\\401A\\sleuth3csv") # or wherever your data are
 
 #######################################################
 # Chapter 3                                         
@@ -9,7 +9,7 @@ names(case0301) = tolower(names(case0301))
 case0301$lrainfall = log(case0301$rainfall)
 
 # Display 3.1 
-by(case0301$rainfall, case0301$treatment, print)
+by(case0301$rainfall, case0301$treatment, function(x) x) 
 
 # Display 3.2
 boxplot( rainfall ~ treatment, case0301)
@@ -19,8 +19,21 @@ boxplot(lrainfall ~ treatment, case0301) # notice the y-axis
 
 # Results on the bottom of page 59 
 t = t.test(lrainfall ~ treatment, case0301, var.equal=TRUE)
+t = t.test(log(rainfall) ~ treatment, case0301, var.equal=TRUE) # This is equivalent
 t
 exp(t$conf.int)
+
+
+# Histograms with overlayed density plots by hand
+# install.packages("ggplot2") # Run this once if you don't have the ggplot2 package installed on your computer
+library(ggplot2) # Load the library for this session of R
+ggplot(case0301, aes(x=lrainfall)) + 
+  geom_histogram(aes(y=..density..), binwidth=0.5, alpha=0.4) + 
+  geom_density(col="red") + facet_wrap(~treatment) 
+# Overlaying the best fitting normal curve is harder
+# see http://stackoverflow.com/questions/1376967/using-stat-function-and-facet-wrap-together-in-ggplot2-in-r
+
+
 
 
 case0302 = read.csv('case0302.csv')
@@ -30,5 +43,7 @@ names(case0302) = tolower(names(case0302))
 boxplot(dioxin~veteran,case0302)
 
 # Results on the top of page 62
-(t = t.test(dioxin~veteran, case0302, var.equal=TRUE, alternative="less"))
--t$conf.int
+(t = t.test(dioxin~veteran, case0302, var.equal=TRUE))
+# Pvalue can be obtained by dividing the two-sided pvalue by 2
+# the confidence interval in the book is the negative of the one 
+# seen here. 
