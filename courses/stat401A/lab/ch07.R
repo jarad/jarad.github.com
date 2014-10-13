6a7607ffb69bc0493118d37baee8dbaac22430ff
Jarad Niemi <niemi@iastate.edu>
2014-10-13 13:37:57 -0500

    rearranged so that case0701 is first and case0702 is second

--- courses/stat401A/lab/ch07.R
+++ courses/stat401A/lab/ch07.R
@@ -1,44 +1,31 @@
 setwd("U:\\401A\\sleuth3csv") # or whever your data are
 
+library(ggplot2)
+
 #######################################################
 # Chapter 7                                         
 #######################################################
 
-bigbang = read.csv("case0701.csv")
-names(bigbang) = tolower(names(bigbang))
-summary(bigbang) 
-pairs(bigbang)
+case0701 = read.csv("case0701.csv")
+names(case0701) = tolower(names(case0701))
+summary(case0701) 
+pairs(case0701)
 
-plot(distance~velocity, bigbang, main='Compare to Display 7.1')
+plot(distance~velocity, case0701, main='Compare to Display 7.1')
 
-carcass = read.csv("case0702.csv")
-names(carcass) = tolower(names(carcass))
-names(carcass)[1] = "hours"
-
-carcass # Compare to Display 7.3
-
-mod = lm(ph~log(hours),carcass)
-plot(ph~hours,carcass, log='x', main="Compare to Display 7.4", pch=19)
-pred = predict(mod, interval="prediction")
-lines(carcass$hours,pred[,1])
-lines(carcass$hours,pred[,2], lty=2)
-lines(carcass$hours,pred[,3], lty=2)
-
-
-
-mod = lm(distance~velocity, bigbang)
+mod = lm(distance~velocity, case0701)
 summary(mod) # Compare to Display 7.9
-bigbang$fit = predict(mod)
-bigbang$res = residual(mod)
-bigbang # Compare to Display 7.8
+case0701$fit = predict(mod)
+case0701$res = residual(mod)
+case0701 # Compare to Display 7.8
 
-plot(distance~velocity, bigbang)
+plot(distance~velocity, case0701)
 abline(mod, col="red")
 
 # centered explanatory variable regression
-bigbang$centered = bigbang$velocity-mean(bigbang$velocity)
-bigbang
-mod2 = lm(distance~centered, bigbang)
+case0701$centered = with(case0701, velocity-mean(velocity)) # Automatically subtract the mean
+case0701
+mod2 = lm(distance~centered, case0701)
 summary(mod2)
 
 # Compare to Display 7.10 and 7.12
@@ -47,10 +34,35 @@ predict(mod2, interval="prediction")      # prediction interval for new data poi
 
 plot(residuals(mod2)~fitted(mod2))        # manual diagnostic plot
 par(mfrow=c(2,3))
-plot(mod2, 1:6, ask=F)                    # automatic diagnostic plot
+plot(mod2, 1:6, ask=F)                    # automatic diagnostic plots
+
+# Sample correlation coefficient
+cor.test(case0701$velocity,case0701$distance)
 
 
 
 
-cor(bigbang$velocity,bigbang$distance)
+
+
+case0702 = read.csv("case0702.csv")
+names(case0702) = tolower(names(case0702))
+names(case0702)[1] = "hours" # To match the SAS code and book results
+
+case0702 # Compare to Display 7.3
+
+# Fit the simple linear regression model
+mod = lm(ph ~ log(hours), case0702)
+
+# This gets confidence band
+ggplot(case0702, aes(x=ph, y=log(hours))) + geom_point() + geom_smooth(method='lm',formula=y~x)
+
+# This gets prediction band
+plot(ph~hours, case0702, log='x', main="Compare to Display 7.4", pch=19)
+pred = predict(mod, interval="prediction")
+lines(case0702$hours,pred[,1])
+lines(case0702$hours,pred[,2], lty=2)
+lines(case0702$hours,pred[,3], lty=2)
+
+
+
 
--- courses/stat401A/lab/ch07.sas
+++ courses/stat401A/lab/ch07.sas
@@ -3,62 +3,69 @@ X 'cd U:\401A\sleuth3csv';
 /*************************************************************/
 /* Chapter 7
 /*************************************************************/
-DATA bigbang;
+DATA case0701;
   INFILE 'case0701.csv' DSD FIRSTOBS=2;
   INPUT velocity distance;
   RUN;
 
-PROC GPLOT DATA=bigbang;
+PROC GPLOT DATA=case0701;
   PLOT distance*velocity;
   TITLE 'Compare to Display 7.1';
   RUN;
 
-DATA carcass;
+/* Compare to Display 7.9 */
+PROC GLM DATA=case0701 PLOT=diagnostics;
+  MODEL distance = velocity;
+  OUTPUT OUT=case0701reg PREDICTED=fitted RESIDUAL=resid; 
+  TITLE 'Compare to Display 7.9';
+  RUN;
+
+PROC PRINT DATA=case0701reg; 
+  TITLE 'Compare to Display 7.8';
+  RUN;
+
+
+/* centering the explanatory variable */
+PROC MEANS DATA=case0701 MEAN;
+  VAR velocity;
+  RUN;
+
+DATA case0701; SET case0701;
+  centered=velocity-373.125; /* Need to manually put in the mean */
+  RUN;
+
+PROC PRINT DATA=case0701; RUN;
+
+PROC GLM DATA=case0701;
+  MODEL distance = centered;
+  RUN;
+
+
+/* Sample correlation coefficient */
+PROC CORR DATA=case0701; RUN;
+
+
+
+DATA case0702;
   INFILE 'case0702.csv' DSD FIRSTOBS=2;
   INPUT hours pH;    /* In the dataset, the first variable is called 'time' */
   lhours = log(hours);
   RUN;
 
 TITLE 'Compare to Display 7.3';
-PROC PRINT DATA=carcass; RUN;
+PROC PRINT DATA=case0702; RUN;
 
-PROC REG DATA=carcass;
+/* PROC REG can only be used for continuous explanatory variables
+   we will use PROC REG later for variable selection */
+PROC REG DATA=case0702;
   MODEL pH = lhours;
   TITLE 'Compare to Display 7.4';
   RUN;
 
-/* Compare to Display 7.9 */
-PROC GLM DATA=bigbang PLOT=diagnostics;
-  MODEL distance = velocity;
-  TITLE 'compare to Display 7.8';
-  OUTPUT OUT=bigbangreg PREDICTED=fitted RESIDUAL=resid;
-  RUN;
-
-PROC PRINT DATA=bigbangreg; RUN;
-
-PROC MEANS DATA=bigbang MEAN;
-  VAR velocity;
-  RUN;
-
-/* centering trick (simple way) */
-DATA bigbang; SET bigbang;
-  centered=velocity-373.125;
-  RUN;
-
-PROC GLM DATA=bigbang;
-  MODEL distance = centered;
-  RUN;
-
-
-PROC PRINT DATA=bigbang; RUN;
-
-/* Sample correlation coefficient */
-PROC CORR DATA=bigbang; RUN;
-
-PROC GLM DATA=carcass;
-  MODEL pH=lhours;
-  OUTPUT OUT=carcassreg LCLM=l95m UCLM=u95m LCL=l95p UCL=u95p;
+PROC GLM DATA=case0702;
+  MODEL pH = lhours;
+  OUTPUT OUT=case0702reg LCLM=l95m UCLM=u95m LCL=l95p UCL=u95p; /* bands for mean and prediction */
   TITLE 'Compare to Displays 7.10 and 7.12';
   RUN;
 
-PROC PRINT DATA=carcassreg; RUN;
+PROC PRINT DATA=case0702reg; RUN;
