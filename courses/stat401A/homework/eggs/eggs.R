6a1b4b9b82e0a443f33a48b5fb4cb8f2771c9ddf
Jarad Niemi <niemi@iastate.edu>
2014-10-15 11:58:10 -0500

    remove html table

--- courses/stat401A/homework/eggs/eggs.R
+++ courses/stat401A/homework/eggs/eggs.R
@@ -2,7 +2,6 @@ d = read.table("eggs.txt", header=TRUE, check.names=FALSE)
 
 library(plyr)
 library(reshape2)
-library(xtable)
 
 m = melt(d)
 sm = ddply(melt(d), .(variable), summarize,
@@ -11,5 +10,4 @@ sm = ddply(melt(d), .(variable), summarize,
       sd = sd(value))
 cor(d)
 
-tab = xtable(sm)
-print(tab, file="table.html", type="html", include.rownames=FALSE)
+sm
