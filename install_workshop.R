c447310159d00e64f928616bd5fb453c03934180
Jarad Niemi <niemi@iastate.edu>
2014-12-03 20:55:32 -0600

    add )

--- install_workshop.R
+++ install_workshop.R
@@ -1,5 +1,5 @@
 install.packages(c("ggplot2", "gridExtra", "knitr", "maps", 
-                   "plyr", "reshape2", "rmarkdown", "SpatialEpi", "xtable")
+                   "plyr", "reshape2", "rmarkdown", "SpatialEpi", "xtable"))
 download.files("https://www.jarad.me/ISDSWorkshop_0.1.tar.gz", "ISDSWorkshop.tar.gz", mode="wb")
 install.packages("ISDSWorkshop.tar.gz", repos = NULL, type = "source")
 
