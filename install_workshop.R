2acc8e8661eba80f316a4b27a295cd2bb9e18f0a
Jarad Niemi <niemi@iastate.edu>
2014-12-04 14:51:29 -0600

    give warning if workshop package is not found

--- install_workshop.R
+++ install_workshop.R
@@ -1,7 +1,12 @@
-install.packages(c("ggplot2", "gridExtra", "knitr", "maps", "mapproj", 
-                   "plyr", "reshape2", "rmarkdown", "SpatialEpi", "xtable"))
+packages = c("ggplot2", "gridExtra", "knitr", "maps", "mapproj", 
+             "plyr", "reshape2", "rmarkdown", "SpatialEpi", "xtable")
+install.packages(packages)
+
 download.file("http://www.jarad.me/ISDSWorkshop_0.1.tar.gz", "ISDSWorkshop.tar.gz", mode="wb")
 install.packages("ISDSWorkshop.tar.gz", repos = NULL, type = "source")
 
-if (require(ISDSWorkshop)) message("\n    ISDS R Workshop installed succesfully!\n")
+installed = packages %in% installed.packages()[,1]
 
+if (any(!installed)) warning("These packages are missing: ", packages[!installed])
+
+if (require(ISDSWorkshop)) message("\n    ISDS R Workshop installed succesfully!\n")
