8852cb094882fdea36b3b5bb20b9405bf5cc498f
Jarad Niemi <niemi@iastate.edu>
2017-01-24 14:00:13 -0600

    Fix lab03 typos

--- courses/stat401Eng/labs/lab03/lab03.R
+++ courses/stat401Eng/labs/lab03/lab03.R
@@ -42,16 +42,20 @@ points(x, dpois(x, lambda = rate), col="red")
 ## ------------------------------------------------------------------------
 a <- 0
 b <- 1
+
+# The curve function expects you to give a function of `x` and then it 
+# (internally) creates a sequence of values from `from` and to `to` and creates
+# plots similar to what we had before, but using a line rather than points.
 curve(dunif(x, min = a, max = b), from = -1, to = 2,
       xlab='y', ylab='f(y)', main='Probability density function for Unif(0,1)')
 
 ## ------------------------------------------------------------------------
 curve(punif(x, min = a, max = b), from = -1, to = 2,
-      xlab='y', ylab='f(y)', main='Cumulative distribution function for Unif(0,1)')
+      xlab='y', ylab='F(y)', main='Cumulative distribution function for Unif(0,1)')
 
 ## ------------------------------------------------------------------------
 curve(qunif(x, min = a, max = b), from = 0, to = 1,
-      xlab='y', ylab='f(y)', main='Quantile function for Unif(0,1)')
+      xlab='p', ylab='F^{-1}(p)', main='Quantile function for Unif(0,1)')
 
 ## ------------------------------------------------------------------------
 random_uniforms <- runif(100, min = a, max = b)
