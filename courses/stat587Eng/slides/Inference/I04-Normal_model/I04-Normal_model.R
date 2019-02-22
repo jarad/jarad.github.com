f81d0af443ac9ce61f0abddba52af05eb6dab432
Jarad Niemi <niemi@iastate.edu>
2019-02-22 11:59:56 -0600

    Update R code

--- courses/stat587Eng/slides/Inference/I04-Normal_model/I04-Normal_model.R
+++ courses/stat587Eng/slides/Inference/I04-Normal_model/I04-Normal_model.R
@@ -1,61 +1,21 @@
 ## ----libraries, message=FALSE, warning=FALSE, cache=FALSE----------------
-library("dplyr")
-library("tidyr")
-library("ggplot2")
+library("tidyverse")
 library("MCMCpack") # for dinvgamma function
 
 ## ----set_seed, echo=FALSE------------------------------------------------
-set.seed(2)
+set.seed(20190222)
 
-## ----data, echo=TRUE-----------------------------------------------------
-m <- 200
-s <- 20
+## ------------------------------------------------------------------------
 n <- 9
-y <- rnorm(n, mean = m, sd = s)
-
-## ----data_plot, echo=TRUE, fig.height=4, dependson="data"----------------
-curve(dnorm(x, mean = mean(y), sd = s/sqrt(n)), mean(y)-3*s/sqrt(n), mean(y)+3*s/sqrt(n), 
-      xlab = expression(mu),
-      ylab = expression(paste("p(",mu,"|y)")),
-      main = "Posterior")
-abline(v=mean(y), col='red')
+d <- data.frame(farm = paste0("farm",1:n), yield = rnorm(n, 200, 25))
+write_csv(d, path="yield.csv")
 
 ## ----echo=TRUE-----------------------------------------------------------
-a <- .05
-qnorm(c(a/2,1-a/2), mean(y), sd = s/sqrt(n))
-
-## ----echo=TRUE, dependson="data"-----------------------------------------
-mean(y) + c(-1,1)*qnorm(1-a/2)*s/sqrt(n)
-
-## ----variance_posterior, echo=TRUE, fig.height=4, dependson="data"-------
-S <- sum((y-m)^2)
-curve(MCMCpack::dinvgamma(x, shape = n/2, scale = S/2), 0, 3*S/n,
-      xlab = expression(sigma^2),
-      ylab = expression(paste("p(",sigma^2,"|y)")),
-      main = "Posterior")
-abline(v = (S/2)/((n/2)-1), col='red')
-
-## ----qinvgamma, echo=TRUE------------------------------------------------
-qinvgamma <- function(p, shape, scale = 1) {
-  1/qgamma(1-p, shape = shape, rate = scale)
-}
-
-## ----ci_for_variance, dependson=c("variance_posterior","qinvgamma","data")----
-(q <- qinvgamma(c(.025,.975), shape = n/2, scale = S/2))
-
-## ----echo=TRUE-----------------------------------------------------------
-draws <- MCMCpack::rinvgamma(1e5, shape = n/2, scale = S/2)
-quantile(draws, c(a/2, 1-a/2))
-
-## ----variance_draws, echo=TRUE-------------------------------------------
-draws <- 1/rgamma(1e5, shape = n/2, rate = S/2)
-quantile( draws, c(a/2, 1-a/2))
-
-## ----sd_posterior, echo=TRUE, fig.height=2.5, dependson="variance_draws"----
-sqrt(q) # Take square root of end points of the CI for the variance
-hist(sqrt(draws), 1001, xlab = expression(sigma), ylab = expression(paste("p(",sigma,"|y)")),
-     main = "Posterior for standard deviation", probability = TRUE)
-abline(v=sqrt(q), col="red")
+d <- read.csv("yield.csv")
+names(d)
+(n <- length(d$yield))
+(ybar <- mean(d$yield))
+(s <- sd(d$yield))
 
 ## ----t_distribution, fig.height=4.5, warning=FALSE-----------------------
 dtms = function(x,v,m=0,s=1) dt((x-m)/s,v)/s
@@ -65,32 +25,66 @@ d = expand.grid(v=10^(0:2), t = seq(-3,6,by=.1)) %>%
          v = factor(v))
 
 ggplot(d, aes(x=t,y=density, group=v, linetype=v, color=v)) +
-  geom_line() + 
+  geom_line() +
   theme(legend.position='bottom') +
   theme_bw()
 
-## ----echo=TRUE-----------------------------------------------------------
-mean(y) + c(-1,1)*qt(.975, df=n-1)*sd(y)/sqrt(n)
+## ----mu_ci, dependson="data", echo=TRUE----------------------------------
+ybar + c(-1,1)*qt(.975, df=n-1)*s/sqrt(n)
 
 ## ----echo=TRUE-----------------------------------------------------------
-a    <- 0.1
-
-mean(y) +c(-1,1)*qt(1-a/2, df=n-1)*sd(y)/sqrt(n)
+pt((200-ybar)/(s/sqrt(n)), df = n-1)
 
 ## ----echo=TRUE-----------------------------------------------------------
-m = 200
-C = 33^2
-Cp = 1/(1/C+n/s^2)
-mp = Cp*(m/C+n*mean(y)/s^2)
+pt((200-ybar)/(s/sqrt(n)), df = n-1) - pt((180-ybar)/(s/sqrt(n)), df = n-1)
+diff(pt((c(180,200)-ybar)/(s/sqrt(n)), df = n-1))
 
-## ----echo=TRUE, fig.height=3.5-------------------------------------------
-ybar = mean(y)
-se = s/sqrt(n)
-curve(dnorm(x, mean=ybar, sd=se), ybar-3*se, ybar+3*se,
-      xlab=expression(mu),
-      ylab=expression(paste("p(",mu,"|y)")),
-      main="Default vs informative Bayesian analysis")
-curve(dnorm(x, mean=mp, sd=sqrt(Cp)), col='red', lty=2, add=TRUE)
-legend("topleft", c("Default","Informative"), col=c("black","red"),
-       lty = 1:2)
+## ----ig_distribution, fig.height=4.5, warning=FALSE----------------------
+d = expand.grid(y = seq(0,10,by=0.1),
+                a = 1:4,
+                b = 1:4) %>%
+  mutate(density = MCMCpack::dinvgamma(y, shape = a, scale = b),
+         af = paste("a=",a), 
+         bf = paste("b=",b))
+
+ggplot(d, aes(x = y, y = density)) +
+  geom_line() +
+  facet_grid(af~bf) +
+  theme_bw()
+
+## ----qinvgamma, echo=TRUE------------------------------------------------
+qinvgamma <- function(p, shape, scale = 1) {
+  1/qgamma(1-p, shape = shape, rate = scale)
+}
+
+## ----echo=TRUE-----------------------------------------------------------
+a <- 0.2
+qinvgamma(c(a/2,1-a/2), shape = (n-1)/2, scale = (n-1)*s^2/2)
+
+## ----pinvgamma, echo=TRUE------------------------------------------------
+pinvgamma <- function(q, shape, scale = 1) {
+  1-pgamma(1/q, shape = shape, rate = scale)
+}
+
+## ----echo=TRUE-----------------------------------------------------------
+pinvgamma(190, shape = (n-1)/2, scale = (n-1)*s^2/2)
+
+## ----echo=TRUE-----------------------------------------------------------
+diff(pinvgamma(c(170,190), shape = (n-1)/2, scale = (n-1)*s^2/2))
+
+## ----echo=TRUE-----------------------------------------------------------
+a <- 0.2
+ci_variance = qinvgamma(c(a/2,1-a/2), shape = (n-1)/2, scale = (n-1)*s^2/2)
+sqrt(ci_variance)
+
+## ----echo=TRUE-----------------------------------------------------------
+pinvgamma(20^2, shape = (n-1)/2, scale = (n-1)*s^2/2)
+
+## ----echo=TRUE-----------------------------------------------------------
+diff(pinvgamma(c(20,25)^2, shape = (n-1)/2, scale = (n-1)*s^2/2))
+
+## ----echo=TRUE-----------------------------------------------------------
+sigma2 <- MCMCpack::rinvgamma(1e5, shape = (n-1)/2, scale = (n-1)*s^2/2)
+c(mean(sigma2), (n-1)*s^2/(n-3)) # estimate first, truth second
+mean(sqrt(sigma2))               # only have an estimate
 
