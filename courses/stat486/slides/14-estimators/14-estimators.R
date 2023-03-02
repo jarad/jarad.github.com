38d7c390453fbd76799169160647a24ad154f029
Jarad Niemi <niemi@iastate.edu>
2023-03-02 09:03:17 -0600

    Add default Bayesian binomial analysis

--- courses/stat486/slides/14-estimators/14-estimators.R
+++ courses/stat486/slides/14-estimators/14-estimators.R
@@ -1,6 +1,5 @@
 ## ---------------------------------------------------------------------------------
 library("tidyverse"); theme_set(theme_bw())
-library("scales") # for breaks_pretty()
 
 
 ## ---------------------------------------------------------------------------------
@@ -19,6 +18,60 @@ p <- 0.3
 
 x <- rbinom(1e3, size = n, prob = p)
 theta_hat = x / n
+
+
+## ---------------------------------------------------------------------------------
+ggplot() + 
+  geom_histogram(mapping = aes(x = theta_hat), bins = 30) +
+  geom_vline(xintercept = p, col = 'red') +
+  labs(title="Sampling distribution for sample proportion")
+
+
+## ---------------------------------------------------------------------------------
+mean(theta_hat)        # expectation
+mean(theta_hat - p)    # bias
+create_ci(theta_hat - p) # bias CI
+
+
+## ---------------------------------------------------------------------------------
+# variance
+mean( (theta_hat - mean(theta_hat))^2 ) 
+
+create_ci( (theta_hat - mean(theta_hat))^2 )
+
+
+## ---------------------------------------------------------------------------------
+# MSE
+mean( (theta_hat - p)^2 )
+
+# MSE CI
+create_ci( (theta_hat-p)^2  )
+
+
+## ---------------------------------------------------------------------------------
+epsilon <- 0.01
+n_seq <- floor(10^seq(1, 5, length = 10))
+p_seq <- numeric(length(n_seq))
+
+for (i in seq_along(n_seq)) {
+  x <- rbinom(1e3, size = n_seq[i], prob = p)
+  p_seq[i] <- mean(abs(x/n_seq[i] - p) > epsilon)
+}
+
+ggplot(data.frame(n = n_seq, p = p_seq),
+       aes(x = n, y = p_seq)) + 
+  geom_point() + 
+  geom_line() +
+  labs(x = "Sample size", y = "Probability",
+       title = "Consistency of sample proportion") 
+
+
+## ---------------------------------------------------------------------------------
+n <- 10
+p <- 0.3
+
+x <- rbinom(1e3, size = n, prob = p)
+theta_hat = (0.5 + x) / (1 + n)
 
 
 ## ---------------------------------------------------------------------------------
@@ -126,8 +179,11 @@ for (i in seq_along(n_seq)) {
   })
   p_seq[i] <- mean( abs(xbar - m) > epsilon)
 }
-
-qplot(n_seq, p_seq, geom = c("point","line")) +
+   
+ggplot(data.frame(n = n_seq, p = p_seq), 
+       aes(x = n, y = p)) +
+  geom_line() +
+  geom_point() + 
   labs(x = "Sample size", y = "Probability",
-       title = "Consistency of sample mean") 
+       title = "Consistency of sample mean")
 
