973e0e3b3311a660dc7f663acb4976fa7eb317b2
Jarad Niemi <niemi@iastate.edu>
2024-09-12 13:45:32 -0500

    Update lab03

--- courses/stat5870Eng/labs/lab03/lab03.R
+++ courses/stat5870Eng/labs/lab03/lab03.R
@@ -5,19 +5,42 @@ b <- 1
 # (internally) creates a sequence of values from `from` and to `to` and creates
 # plots similar to what we had before, but using a line rather than points.
 curve(dunif(x, min = a, max = b), 
-      from = -1, to = 2,
+      from = -1, to = 2, n = 1001, 
       xlab='y', ylab='f(y)', 
       main='Probability density function for Unif(0,1)')
 
 curve(punif(x, min = a, max = b), 
-      from = -1, to = 2,
+      from = -1, to = 2, n = 1001,
       xlab='y', ylab='F(y)', 
       main='Cumulative distribution function for Unif(0,1)')
 
-curve(qunif(x, min = a, max = b), 
-      from = 0, to = 1,
-      xlab='p', ylab='F^{-1}(p)', 
-      main='Quantile function for Unif(0,1)')
+# Visualize the CDF
+curve(dunif(x, min = a, max = b), 
+      from = -1, to = 2, n = 1001,
+      xlab='y', ylab='f(y)', 
+      main='CDF area under the curve (integral)')
+
+U <- 0.3
+x <- seq(-1, U, length = 1001)
+polygon(c(x, rev(x)), 
+          c(rep(0, length(x)), dunif(rev(x), min = a, max = b)),
+          col = "red", lty = 0)
+
+punif(U, min = a, max = b)
+
+# Visualize area under the curve
+curve(dunif(x, min = a, max = b), 
+      from = -1, to = 2, n = 1001,
+      xlab='y', ylab='f(y)', 
+      main='Area under the curve (integral)')
+
+L <- 0.3; U <- 0.7
+x <- seq(L, U, length = 1001)
+polygon(c(x, rev(x)), 
+          c(rep(0, length(x)), dunif(rev(x), min = a, max = b)),
+          col = "red", lty = 0)
+
+punif(U, min = a, max = b) - punif(L, min = a, max = b)
 
 random_uniforms <- runif(100, min = a, max = b)
 
@@ -34,19 +57,46 @@ mu    <- 0
 sigma <- 1 # standard deviation
 
 curve(dnorm(x, mean = mu, sd = sigma), # notice the 3rd argument is the sd
-      from = -4, to = 4,
+      from = mu - 4*sigma, to = mu + 4*sigma, n = 1001,
       main = "PDF for a standard normal")
 
 curve(pnorm(x, mean = mu, sd = sigma), 
-      from = -4, to = 4,
+      from = mu - 4*sigma, to = mu + 4*sigma, n = 1001,
       main = "CDF for a standard normal",
       ylab = "F(x)")
 
+# Visualize the CDF
+curve(dnorm(x, mean = mu, sd = sigma), 
+      from = mu - 4*sigma, to = mu + 4*sigma, n = 1001,
+      xlab='y', ylab='f(y)', 
+      main='Integral')
+
+x <- seq(mu - 4*sigma, 0.3, length = 1001)
+polygon(c(x, rev(x)), 
+          c(rep(0, length(x)), dnorm(rev(x), mean = mu, sd = sigma)),
+          col = "red", lty = 0)
+
+pnorm(0.3, mean = mu, sd = sigma)
+
+# Visualize area under the curve
+curve(dnorm(x, mean = mu, sd = sigma), 
+      from = mu - 4*sigma, to = mu + 4*sigma, n = 1001,
+      xlab='y', ylab='f(y)', 
+      main='Integral')
+
+L <- -0.2; U <- 0.3
+x <- seq(L, U, length = 1001)
+polygon(c(x, rev(x)), 
+          c(rep(0, length(x)), dnorm(rev(x), mean = mu, sd = sigma)),
+          col = "red", lty = 0)
+
+pnorm(U, mean = mu, sd = sigma) - pnorm(L, mean = mu, sd = sigma)
+
 curve(qnorm(x, mean = mu, sd = sigma),
-      from = 0, to = 1, 
+      from = 0, to = 1, n = 1001, 
       main = "Quantile function for a standard normal")
 
 draws <- rnorm(100, mean = mu, sd = sigma)
 hist(draws, probability = TRUE)
-curve(dnorm(x, mean = mu, sd = sigma), 
+curve(dnorm(x, mean = mu, sd = sigma), n = 1001, 
       add = TRUE, col = "red")
