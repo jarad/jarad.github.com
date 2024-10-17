d684dc1a8d8f27f312b6965f69b908e605a7cb76
Jarad Niemi <niemi@iastate.edu>
2024-10-17 13:42:23 -0500

    Add RMarkdown lab

--- courses/stat5870Eng/labs/index.md
+++ courses/stat5870Eng/labs/index.md
@@ -14,14 +14,16 @@ tagline: Statistical Methods for Research Workers
  - [Lab04](lab04/lab04.html) - Graphical Statistics
  - [Lab05](lab05/lab05.html) - Estimator Evaluation
  - [Lab06](lab06/lab06.html) - Built-in Analyses in R
- - [Lab07](lab07/lab07.html) - Default Regression Diagnostics
+ - [Lab07](lab07/lab07.html) - RMarkdown
+ 
+   === updated through here ===
+ 
  - [Lab08](lab08/lab08.html) - Categorical Explanatory Variables
  - [Lab09](lab09/lab09.html) - Multiple Regression in R
  - [Lab10](lab10/lab10.html) - ANOVA and F-tests
  - [Lab11](lab11/lab11.html) - Contrasts
  - [Lab12](lab12/lab12.html) - Generalized linear mixed effect models
  
-   === updated through here ===
   
  - [LabMC](lab04_old/lab04_old.Rmd) - Monte Carlo Methods
  - [LabData](labData/lab02.html) - Data Management in R
--- courses/stat5870Eng/labs/lab07/lab07.R
+++ courses/stat5870Eng/labs/lab07/lab07.R
@@ -1,280 +1,73 @@
-## -----------------------------------------------------------------------------
-if (!require("tidyverse")) {
-  install.packages("tidyverse")
-  library("tidyverse")
-}
-
-
-## -----------------------------------------------------------------------------
-set.seed(20170320)
-
-
-## -----------------------------------------------------------------------------
-n <- 100 
-x <- rnorm(n)
-b0 <- 1
-b1 <- 2
-sigma <- 0.4
-error <- rnorm(n,0,sigma)
-y <- b0+b1*x+error
-
-d <- data.frame(x=x,y=y)
-
-
-## -----------------------------------------------------------------------------
-plot(y ~ x, data = d)
-
-
-## -----------------------------------------------------------------------------
-library("ggplot2")
-ggplot(d, aes(x=x, y=y)) +
-  geom_point() +
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-m <- lm(y ~ x, data = d)
-
-
-## -----------------------------------------------------------------------------
-m
-summary(m)
-
-
-## -----------------------------------------------------------------------------
-plot(y ~ x, data = d)
-abline(m, col='blue')
-
-
-## -----------------------------------------------------------------------------
-ggplot(d, aes(x=x, y=y)) +
-  geom_point() + 
-  stat_smooth(method = "lm", se = FALSE) +
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-opar = par(mfrow=c(2,3))  # Create a 2x3 grid of plots and save the original settings
-plot(m, 1:6, ask=FALSE)   # Plot all 6 diagnostics plots
-par(opar)                 # Return to original graphics settings
-
-
-
-
-## -----------------------------------------------------------------------------
-n <- 100
-errors <- data.frame(rep = 1:n,
-                     normal       = rnorm(n),
-                     heavy_tailed = rt(n, df=3),
-                     right_skewed = exp(rnorm(n))) %>%
-  
-  mutate(right_skewed = right_skewed - exp(1/2),  # make sure errors have expectation of 0
-         left_skewed  = 0 - right_skewed) 
-
-errors_long <- errors %>%
-  tidyr::gather(distribution,value,-rep) %>%
-  mutate(distribution = factor(distribution, 
-                               levels = c("normal",
-                                          "heavy_tailed",
-                                          "right_skewed",
-                                          "left_skewed")))
-
-
-## -----------------------------------------------------------------------------
-ggplot(errors_long, aes(x=value)) + 
-  geom_histogram() + 
-  facet_grid(. ~ distribution) + 
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-ggplot(errors_long, aes(sample=value)) + 
-  geom_qq() + 
-  geom_qq_line() + 
-  facet_grid(. ~ distribution) + 
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-opar = par(mfrow=c(1,4))
-qqnorm(errors$normal, main="Normal")
-qqline(errors$normal)
-qqnorm(errors$heavy_tailed, main="Heavy-tailed")
-qqline(errors$heavy_tailed)
-qqnorm(errors$right_skewed, main="Right-skewed")
-qqline(errors$right_skewed)
-qqnorm(errors$left_skewed, main="Left-skewed")
-qqline(errors$left_skewed)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-y <- errors %>%
-  mutate(x = rnorm(n))
-
-models <- list()
-models[[1]] <- lm(normal       ~ x, data=y)
-models[[2]] <- lm(heavy_tailed ~ x, data=y)
-models[[3]] <- lm(right_skewed ~ x, data=y)
-models[[4]] <- lm(left_skewed  ~ x, data=y)
-names(models) <- c("normal","heavy_tailed","right_skewed","left_skewed")
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(models$normal, 1:6, ask=FALSE)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(models$heavy_tailed, 1:6, ask=FALSE)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(models$right_skewed, 1:6, ask=FALSE)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(models$left_skewed, 1:6, ask=FALSE)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-opar = par(mfrow=c(1,4))
-for (i in 1:4) plot(models[[i]], 2, main=names(models)[i])
-par(opar)
-
-
-
-
-## -----------------------------------------------------------------------------
-n  <- 100
-b0 <- 0
-b1 <- 1
-d  <- data.frame(x = runif(n)) %>%
-  mutate(error = rnorm(n,0,x),
-         y = b0+b1*x+error)
-  
-m <- lm(y~x, data=d)
-opar <- par(mfrow=c(2,3))
-plot(m, 1:6, ask=FALSE)
-par(opar)
-
-
-## -----------------------------------------------------------------------------
-plot(m, 1)
-
-
-## -----------------------------------------------------------------------------
-plot(m,3)
-
-
-
-
-## -----------------------------------------------------------------------------
-n <- 100
-b0 <-1 
-b1 <- 2
-d <- data.frame(x = rnorm(n)) %>%
-  mutate(
-    error =rnorm(n),
-    y = b0+b1*x + error)
-
-m <- lm(y ~ x, data = d)
-
-
-## -----------------------------------------------------------------------------
-names(m)
-
-
-## -----------------------------------------------------------------------------
-plot(m$residuals)
-
-
-## -----------------------------------------------------------------------------
-dd <- data.frame(index = 1:length(m$residuals),
-                 residuals = m$residuals)
-ggplot(dd, aes(index, residuals)) +
-  geom_point() + 
-  stat_smooth(se = FALSE) +
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-d <- d %>% 
-  mutate(error = sin(2*pi*(1:n)/n)+rnorm(n),
-         y = b0+b1*x+error)
-
-m <- lm(y ~ x, data = d)
-
-dd <- data.frame(index = 1:length(m$residuals),
-                 residuals = m$residuals)
-ggplot(dd, aes(index, residuals)) +
-  geom_point() + 
-  stat_smooth(se = FALSE) +
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(m, 1:6, ask=FALSE)
-par(opar)
-
-
-
-
-## -----------------------------------------------------------------------------
-n <- 100
-b0 <- 1
-b1 <- 2
-d <- data.frame(x = rnorm(n)) %>%
-  mutate(y = b0+b1*x + rnorm(n))
-                
-m <- lm(y ~ x, data = d)
-
-d$residuals <- m$residuals
-
-ggplot(d, aes(x, residuals)) + 
-  geom_point() + 
-  stat_smooth(se = FALSE) + 
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-n <- 100
-b0 <- 1
-b1 <- 2
-d <- data.frame(x = rnorm(n)) %>%
-  mutate(y = b0+b1*x+x^2 + rnorm(n))
-                
-m <- lm(y ~ x, data = d)
-
-d$residuals <- m$residuals
-
-ggplot(d, aes(x, residuals)) + 
-  geom_point() + 
-  stat_smooth(se = FALSE) + 
-  theme_bw()
-
-
-## -----------------------------------------------------------------------------
-opar <- par(mfrow=c(2,3))
-plot(m, 1:6, ask=FALSE)
-par(opar)
-
-
-
-
-## ----warning=FALSE------------------------------------------------------------
-# install.packages("ggResidpanel")
-library("ggResidpanel")
-
-resid_panel(m, plots="R")
-# resid_panel(m, plots="SAS")
-resid_xpanel(m)
-
+# Author: Jarad Niemi
+# Date:   2024-10-17
+# Purpose: Introduce RMarkdown file format using Module 2 content
+#-------------------------------------------------------------------------------
+
+
+library("tidyverse")
+library("Sleuth3")   # data
+library("rmarkdown") # compiling RMarkdown files
+
+case0401
+
+n <- sum(case0401$Launch == "Warm")
+y <- sum(case0401$Incidents[case0401$Launch == "Warm"] == 0)
+
+## Frequentist analysis
+binom.test(y, n)
+prop.test( y, n) # based on CLT (not reasonable here)
+
+binom.test(y, n)$conf.int # Q1.5
+
+# Bayesian analysis
+(1 + y) / (2 + n)
+qbeta(c(.025, .975), 1 + y, 1 + n - y) # Credible interval
+1 - pbeta(0.9, 1 + y, 1 + n - y)       # P(theta > 0.9 | y)
+
+extrinsic <- case0101 |> 
+  filter(Treatment == "Extrinsic") 
+
+summary(extrinsic)
+
+t.test(extrinsic$Score)
+
+# Compute summary statistics from data
+d <- case0401 |>
+  group_by(Launch) |>
+  summarize(
+    n = n(),
+    y = sum(Incidents == 0),
+    p = y/n
+  )
+
+d
+
+prop.test(d$y, d$n)
+
+# Simulate from the posterior for each probability of success
+nreps      <- 1e5                                           # make this large
+prob_cool <- rbeta(nreps, 1 + d$y[1], 1 + d$n[1] - d$y[1]) 
+prob_warm <- rbeta(nreps, 1 + d$y[2], 1 + d$n[2] - d$y[2])
+diff       <- prob_cool - prob_warm
+
+# Calculate quantities of interest
+mean(diff < 0)                          # P(prob_warm > prob_cool | y)
+quantile(-diff, probs = c(.025, 0.975)) # 95% credible interval for prob_warm - prob_cool
+
+# Summary statistics
+case0101 |>
+  group_by(Treatment) |>
+  summarize(
+    n    = n(),
+    mean = mean(Score),
+    sd   = sd(Score)
+  )
+
+ggplot(case0101,
+       aes(x = Treatment, 
+           y = Score,
+           color = Treatment,
+           shape = Treatment)) +
+  geom_jitter(width = 0.1)       # jitter the points makes sure points don't overlap
+
+(tt <- t.test(Score ~ Treatment, data = case0101, var.equal = TRUE))
