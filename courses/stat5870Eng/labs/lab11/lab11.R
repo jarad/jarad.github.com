ed0e2f71441ef46cb4aba41d740c280af3f15d4a
Jarad Niemi <niemi@iastate.edu>
2024-12-05 12:57:13 -0600

    Update lab 11

--- courses/stat5870Eng/labs/lab11/lab11.R
+++ courses/stat5870Eng/labs/lab11/lab11.R
@@ -1,6 +1,7 @@
 # Author: Jarad Niemi
-# Date:   2024-11-22
-# Purpose: Contrasts using the emmeans package
+# Date:   2024-12-05
+# Purpose: Contrasts using the emmeans package including built-in comparisons
+#          and custom comparisons.
 #-------------------------------------------------------------------------------
 
 
@@ -9,30 +10,33 @@
 library("tidyverse"); theme_set(theme_bw())
 library("emmeans")
 
-ggplot(emmeans::fiber, aes(machine, strength)) + 
-  geom_jitter() + 
-  theme_bw()
+ggplot(emmeans::fiber, 
+       aes(
+         x = machine, 
+         y = strength)) + 
+  geom_jitter(width = 0.1) 
 
 m <- lm(strength ~ machine, data = emmeans::fiber)
-nd <- data.frame(machine = c("A","B","C"))
+nd <- data.frame(machine = c("A", "B", "C"))
 p <- predict(m, 
-             newdata = nd, 
+             newdata  = nd, 
              interval = "confidence")
+
 bind_cols(nd, p |> as.data.frame())
 
-emmeans(m, ~machine)
+emmeans(m, ~ machine)
 
 ex0518 <- Sleuth3::ex0518 |>
-  mutate(Treatment = relevel(Treatment, ref="Control"))
+  mutate(Treatment = relevel(Treatment, ref = "Control"))
 
 ggplot(ex0518, aes(Treatment, Protein)) + 
-  geom_jitter()
+  geom_jitter(width = 0.1)
 
 
 
 # First let's make C the reference level
 fiber <- emmeans::fiber |> 
-  mutate(machine = relevel(machine, ref="C"))
+  mutate(machine = relevel(machine, ref = "C"))
   
 m <- lm(strength ~ machine, data = fiber)
 em <- emmeans(m, ~ machine)
@@ -48,23 +52,29 @@ confint(co)
 
 
 
-wood_glue <- data.frame(weight = c(185,170,210,240,245,190,210,250,
-                                  290,280,260,270,200,280,350,350),
-                        wood = rep(c("spruce","maple"), each = 8),
-                        glue = rep(c("carpenter's","weldbond",
-                                     "gorilla","titebond"), 
-                                   each=2, times=2))
+# Construct wood glue data frame
+wood_glue <- data.frame(weight = c(185, 170, 210, 240, 245, 190, 210, 250,
+                                  290, 280, 260, 270, 200, 280, 350, 350),
+                        wood = rep(c("spruce", "maple"), each = 8),
+                        glue = rep(c("carpenter's", "weldbond",
+                                     "gorilla", "titebond"), 
+                                   each = 2, times = 2))
 
-ggplot(wood_glue, aes(wood, weight, color=glue, shape=glue)) +
-  geom_jitter() +
-  theme_bw()
+# Plot wood glue data set
+ggplot(wood_glue, 
+       aes(
+         x     = wood, 
+         y     = weight, 
+         color = glue, 
+         shape = glue)) +
+  geom_jitter(width = 0.1)
 
-m <- lm(weight ~ wood*glue, data = wood_glue)
+m <- lm(weight ~ wood * glue, data = wood_glue)
 anova(m)
 
 (em <- emmeans(m, ~ glue))
 
-(co <- contrast(em, "pairwise"))
+(co <- contrast(em, method = "pairwise"))
 confint(co)
 
 (em <- emmeans(m, ~ glue | wood))
@@ -76,11 +86,15 @@ confint(co)
 
 
 
-ggplot(emmeans::fiber, aes(diameter, strength, color=machine, shape=machine)) + 
-  geom_jitter() + 
-  theme_bw()
+ggplot(emmeans::fiber, 
+       aes(
+         x     = diameter, 
+         y     = strength, 
+         color = machine, 
+         shape = machine)) + 
+  geom_jitter(width = 0.1) 
 
-m <- lm(strength ~ diameter*machine, data=fiber)
+m <- lm(strength ~ diameter * machine, data = fiber)
 em <- emmeans(m, ~ machine)
 co <- contrast(em, method = "pairwise")
 confint(co)
